89030df368739e3268896ca479b58c5ddf6897ac
Sachin Parmar <psachin.parmar@gmail.com>
2022-08-04 11:26:15 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package LetsLearnGit;
+
+public class Multiply{
+    
+    public static void main(String[] args){
+        int a =5;
+        int b =6;
+
+        System.out.println("Product is: "+ (a*b));
+    }
+}
